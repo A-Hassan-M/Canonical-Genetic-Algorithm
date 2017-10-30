e35846cbdaa43a6f5bdedbbae5c19e35757919ca
A-Hassan-M <mido20140014@gmail.com>
2017-10-31 00:14:02 +0200

    GA

--- CurveFitting with GA/src/FloatingPointGA.java
+++ CurveFitting with GA/src/FloatingPointGA.java
@@ -1,67 +1,111 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class FloatingPointGA{
-	protected ArrayList<Solution> population;
-	protected ArrayList<Point> points;
-	protected int degree;
-	protected final double P_MUTAION = 0.01;
-	protected final double P_COSSOVER = 0.64;
-	protected final int POPULATION_SIZE = 1000;
-	protected final int GENERATION_SIZE = 700;
+	private ArrayList<Solution> population;
+	private ArrayList<Point> points;
+	private int degree;
+	private final double P_MUTAION = 0.01;
+	private final double P_COSSOVER = 0.64;
+	private final int POPULATION_SIZE = 1000;
+	private final int GENERATION_SIZE = 700;
+	
+	
 	public FloatingPointGA(ArrayList<Point> points,int degree) {
 		this.points = points;
 		this.degree = degree;
 	}
-
 	
-	protected boolean isValid(ArrayList<Double> child) {
-		// TODO Auto-generated method stub
-		return false;
+	public Solution solve() {
+		int i =0;
+		generatePopulation();
+		while (i++<GENERATION_SIZE) {
+			calculateFitness();
+			double[] rouletteWheel = getRouletteWheel();
+			ArrayList<Solution> selectedSol = select(rouletteWheel);
+			ArrayList<Solution> offSpring = Xover(selectedSol);
+			ArrayList<Solution> mutated = mutate(offSpring);
+			replace(mutated);
+		}
+		calculateFitness();
+		return getBest();
 	}
 
 	
-	protected Solution getBest() {
+	private Solution getBest() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
-	protected void generatePopulation() {
-		// TODO Auto-generated method stub
+	private void generatePopulation() {
+		for (int i = 0; i < POPULATION_SIZE; i++) {
+
+			Random randGenerator = new Random();
+			ArrayList<Double> temp = new ArrayList<Double>();
+			for (int j = 0; j <= degree; j++) {
+				double randCell = -10 + (20) * randGenerator.nextDouble();
+				temp.add(randCell);
+			}
+			population.add(new Solution(0, temp));
+		}
 		
 	}
 
 	
-	protected void calculateFitness() {
-		
+	private void calculateFitness() {
+		for (int i = 0; i < population.size(); i++) {
+			ArrayList<Double> solution = population.get(i).getChromosome();
+			double fitness = 0, squareError = 0;
+			
+			for (int j = 0; j < points.size(); j++) {
+				double hY = func(points.get(j),solution);
+				squareError += (points.get(j).y - hY)*(points.get(j).y - hY);
+			}
+			
+			squareError = (1.0/points.size())*squareError;
+			fitness = 1.0/squareError;
+			
+			population.get(i).setError(squareError);
+			population.get(i).setFitness(fitness);
+
+		}
 	}
 
 	
-	protected double[] getRouletteWheel() {
+	private double func(Point point, ArrayList<Double> solution) {
+		double y = solution.get(0);
+		for(int i=1;i<solution.size();i++) {
+			y += solution.get(i)*Math.pow(point.x,i);
+		}
+		return y;
+	}
+
+	private double[] getRouletteWheel() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
-	protected ArrayList<Solution> select(double[] rouletteWheel) {
+	private ArrayList<Solution> select(double[] rouletteWheel) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
-	protected ArrayList<Solution> Xover(ArrayList<Solution> selectedSol) {
+	private ArrayList<Solution> Xover(ArrayList<Solution> selectedSol) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
-	protected ArrayList<Solution> mutate(ArrayList<Solution> offSpring) {
+	private ArrayList<Solution> mutate(ArrayList<Solution> offSpring) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
-	protected void replace(ArrayList<Solution> mutated) {
+	private void replace(ArrayList<Solution> mutated) {
 		// TODO Auto-generated method stub
 		
 	}
--- CurveFitting with GA/src/Solution.java
+++ CurveFitting with GA/src/Solution.java
@@ -3,14 +3,10 @@ import java.util.ArrayList;
 public class Solution {
 	private double fitness;
 	private ArrayList<Double> chromosome;
-	private int value;
-	private int weight;
-	int numOfItems;
+	private double error;
 	
 	
 	public Solution(){
-		value = 0;
-		weight = 0;
 	}
 	public Solution(double fitness, ArrayList<Double> chromosome) {
 		this.fitness = fitness;
@@ -24,11 +20,8 @@ public class Solution {
 	public void setChromosome(ArrayList<Double> chromosome) {
 		this.chromosome = chromosome;
 	}
-	public void setValue(int value) {
-		this.value = value;
-	}
-	public void setWeight(int weight) {
-		this.weight = weight;
+	public void setError(double error) {
+		this.error = error;
 	}
 	
 	//Getters
@@ -38,11 +31,8 @@ public class Solution {
 	public ArrayList<Double> getChromosome() {
 		return chromosome;
 	}
-	public int getValue() {
-		return value;
-	}
-	public int getWeight() {
-		return weight;
+	public double getError() {
+		return error;
 	}
 
 }
